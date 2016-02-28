1bc23884a107333f01c323ed6ff3213360926625
Meggan Turner <meggan.turner@hotmail.com>
2016-02-28 23:04:47 +1100

    taurus #starinfo complete

--- js/main.js
+++ js/main.js
@@ -1,45 +1,88 @@
 $(".botein").click(function(){
-   $("#starinfo").html("<p class='exit'>x</p><h4>Botein</h4><p>Botein is a star in the northern constellation Aries. The name is derived from the Arabic word for 'belly'.</p><table><tr><td>Apparent Magnitude</td><td>4.349</td></tr><tr><td>Spectral type</td><td>K2 III</td></tr><tr><td>Mass</td><td>1.91 Mol</td></tr><tr><td>Luminosity</td><td>45</td></tr><tr><td>Distance from Earth</td><td>170ly / 52pc</td></tr><tr><td>Temperature</td><td>4810 K</td></tr></table>")
+   $("#starinfo").html("<h5>x</h5><h4>Botein</h4><p>Botein is a star in the northern constellation Aries. The name is derived from the Arabic word for 'belly'.</p><table><tr><td>Apparent Magnitude</td><td>4.349</td></tr><tr><td>Spectral type</td><td>K2 III</td></tr><tr><td>Mass</td><td>1.91 Mol</td></tr><tr><td>Luminosity</td><td>45</td></tr><tr><td>Distance from Earth</td><td>170ly / 52pc</td></tr><tr><td>Temperature</td><td>4810 K</td></tr></table>")
       .fadeIn(3000);
 });
 
 $(".hamal").click(function(){
-   $("#starinfo").html("<h4>Hamal</h4><p>Hamal is the brightest star in the Aries constellation, and among the brightest stars in the night sky. It is a giant star that may host an orbiting planet with a mass greater than Jupiter.</p><table><tr><td>Apparent Magnitude</td><td>2.00</td></tr><tr><td>Spectral type</td><td>K1 IIIb</td></tr><tr><td>Mass</td><td>1.5 Mol</td></tr><tr><td>Luminosity</td><td>91</td></tr><tr><td>Distance from Earth</td><td>65.8ly / 20.2pc</td></tr><tr><td>Temperature</td><td>4480 K</td></tr></table>")
+   $("#starinfo").html("<h5>x</h5><h4>Hamal</h4><p>Hamal is the brightest star in the Aries constellation, and among the brightest stars in the night sky. It is a giant star that may host an orbiting planet with a mass greater than Jupiter.</p><table><tr><td>Apparent Magnitude</td><td>2.00</td></tr><tr><td>Spectral type</td><td>K1 IIIb</td></tr><tr><td>Mass</td><td>1.5 Mol</td></tr><tr><td>Luminosity</td><td>91</td></tr><tr><td>Distance from Earth</td><td>65.8ly / 20.2pc</td></tr><tr><td>Temperature</td><td>4480 K</td></tr></table>")
       .fadeIn(3000);
 });
 
 $(".sheratan").click(function (){
-   $("#starinfo").html("<h4>Sheratan</h4><p>Sheratan is a binary star in the Aries constellation. It marks the Ram's second horn.</p><table><tr><td>Apparent Magnitude</td><td>2.655</td></tr><tr><td>Spectral type</td><td>A5 V</td></tr><tr><td>Mass</td><td>2.34 Mol</td></tr><tr><td>Luminosity</td><td>23</td></tr><tr><td>Distance from Earth</td><td>59.6ly / 18.3pc</td></tr><tr><td>Temperature</td><td>4810 K</td></tr></table>")
+   $("#starinfo").html("<h5>x</h5><h4>Sheratan</h4><p>Sheratan is a binary star in the Aries constellation. It marks the Ram's second horn.</p><table><tr><td>Apparent Magnitude</td><td>2.655</td></tr><tr><td>Spectral type</td><td>A5 V</td></tr><tr><td>Mass</td><td>2.34 Mol</td></tr><tr><td>Luminosity</td><td>23</td></tr><tr><td>Distance from Earth</td><td>59.6ly / 18.3pc</td></tr><tr><td>Temperature</td><td>4810 K</td></tr></table>")
       .fadeIn(3000);
 })
 
 $(".mesarthim").click(function(){
-   $("#starinfo").html("<h4>Mesarthim</h4><p>Hamal is a binary star system in the northern constellation of Aries. </p><table><tr><td>Apparent Magnitude</td><td>3.86</td></tr><tr><td>Spectral type</td><td>B9 V</td></tr><tr><td>Mass</td><td>Unkown Mol</td></tr><tr><td>Luminosity</td><td>Unknown</td></tr><tr><td>Distance from Earth</td><td>164ly / 50pc</td></tr><tr><td>Temperature</td><td>10970 K</td></tr></table>")
+   $("#starinfo").html("<h5>x</h5><h4>Mesarthim</h4><p>Hamal is a binary star system in the northern constellation of Aries. </p><table><tr><td>Apparent Magnitude</td><td>3.86</td></tr><tr><td>Spectral type</td><td>B9 V</td></tr><tr><td>Mass</td><td>Unkown Mol</td></tr><tr><td>Luminosity</td><td>Unknown</td></tr><tr><td>Distance from Earth</td><td>164ly / 50pc</td></tr><tr><td>Temperature</td><td>10970 K</td></tr></table>")
       .fadeIn(3000);
 })
 
 $(".41ari").click(function(){
-   $("#starinfo").html("<h4>41 Arietes</h4><p>41 Arietes (sometimes abbreviated to 41 Ari), is a binary star in the northern constellation of Aries.</p><table><tr><td>Apparent Magnitude</td><td>3.63</td></tr><tr><td>Spectral type</td><td>B8 Vn</td></tr><tr><td>Mass</td><td>3.1 Solar Masses</td></tr><tr><td>Luminosity</td><td>160 Solar Luminosity</td></tr><tr><td>Distance from Earth</td><td>166ly / 50.8pc</td></tr><tr><td>Temperature</td><td>11900 K</td></tr></table>")
+   $("#starinfo").html("<h5>x</h5><h4>41 Arietes</h4><p>41 Arietes (sometimes abbreviated to 41 Ari), is a binary star in the northern constellation of Aries.</p><table><tr><td>Apparent Magnitude</td><td>3.63</td></tr><tr><td>Spectral type</td><td>B8 Vn</td></tr><tr><td>Mass</td><td>3.1 Solar Masses</td></tr><tr><td>Luminosity</td><td>160 Solar Luminosity</td></tr><tr><td>Distance from Earth</td><td>166ly / 50.8pc</td></tr><tr><td>Temperature</td><td>11900 K</td></tr></table>")
       .fadeIn(3000);
 })
 
-$()
 
-$(document).click(function (event) {
-   if (event.target.id !== "#starinfo") {
-      $("#starinfo").fadeOut();
-   }
+$(".omicron-tauri").click(function(){
+   $("#starinfo").html("<h4>Omicron Tauri</h4><p>Omicron Tauri is a binary star system with two components orbiting each other over a period of 1655 days.</p><table><tr><td>Apparent Magnitude</td><td>4.875</td></tr><tr><td>Spectral type</td><td>B2 V</td></tr><tr><td>Mass</td><td>3.0 Solar Masses</td></tr><tr><td>Luminosity</td><td>Unknown</td></tr><tr><td>Distance from Earth</td><td>290ly / 89pc</td></tr><tr><td>Temperature</td><td>20700 K</td></tr></table>")
+      .fadeIn(3000);
+})
+$(".xi-tauri").click(function(){
+   $("#starinfo").html("<h4>Xi Tauri</h4><p>Xi Tauri is a hierarchical quadruple system in the contstellation Taurus. Its proper name, Ushakaron, is Akkadian for the 'Exactor of Justice and Retribution'.</p><table><tr><td>Apparent Magnitude</td><td>3.73</td></tr><tr><td>Spectral type</td><td>B8Vn</td></tr><tr><td>Mass</td><td>n/a</td></tr><tr><td>Luminosity</td><td>n/a</td></tr><tr><td>Distance from Earth</td><td>210ly / 64pc</td></tr><tr><td>Temperature</td><td>20700 K</td></tr></table>")
+      .fadeIn(3000);
+})
+$(".pectus-tauri").click(function(){
+   $("#starinfo").html("<h4>Pectus Tauri</h4><p>Pectus Tauri, or Lambda Tauri, is a triple star system in the constellation Taurus. Pectus Tauri translates to 'The Bull Chest'.</p><table><tr><td>Apparent Magnitude</td><td>3.47</td></tr><tr><td>Spectral type</td><td>B3 V</td></tr><tr><td>Mass</td><td>7.18 Solar Masses</td></tr><tr><td>Luminosity</td><td>5801 Solar Luminosity</td></tr><tr><td>Distance from Earth</td><td>480ly / 148pc</td></tr><tr><td>Temperature</td><td>18700 K</td></tr></table>")
+      .fadeIn(3000);
+})
+$(".gamma-tauri").click(function(){
+   $("#starinfo").html("<h4>Gamma Tauri</h4><p>Gamma Tauri, or Hyadum I, is a star which is approximately 470 million years old. It has passed through the main sequence stage and is now on the red giant branch.</p><table><tr><td>Apparent Magnitude</td><td>3.654</td></tr><tr><td>Spectral type</td><td>G8III</td></tr><tr><td>Mass</td><td>2.7 Solar Masses</td></tr><tr><td>Luminosity</td><td>85 Solar Luminosity</td></tr><tr><td>Distance from Earth</td><td>154ly / 47pc</td></tr><tr><td>Temperature</td><td>4844 K</td></tr></table>")
+      .fadeIn(3000);
+})
+$(".theta-tauri").click(function(){
+   $("#starinfo").html("<h4>Theta Tauri</h4><p>Theta Tauri is a double star in the Taurus constellation and a member of the Hyades open cluster.</p><table><tr><td>Apparent Magnitude</td><td>3.84</td></tr><tr><td>Spectral type</td><td>K0 IIIb Fe-0.5</td></tr><tr><td>Mass</td><td>n/a</td></tr><tr><td>Luminosity</td><td>n/a</td></tr><tr><td>Distance from Earth</td><td>154ly / 47.3pc</td></tr><tr><td>Temperature</td><td>n/a</td></tr></table>")
+      .fadeIn(3000);
+})
+$(".delta-tauri").click(function(){
+   $("#starinfo").html("<h4>Delta Tauri</h4><p>Delta Tauris is the name shared by three star systems in the constellation Taurus, which are also members of the Hyades star cluster.</p><table><tr><td>Apparent Magnitude</td><td>n/a</td></tr><tr><td>Spectral type</td><td>n/a</td></tr><tr><td>Mass</td><td>n/a</td></tr><tr><td>Luminosity</td><td>n/a</td></tr><tr><td>Distance from Earth</td><td>146-153ly / 45-47pc</td></tr><tr><td>Temperature</td><td>n/a</td></tr></table>")
+      .fadeIn(3000);
+})
+$(".aldebaran").click(function(){
+   $("#starinfo").html("<h4>Aldebaran</h4><p>Aldebaran is the brightest star in the Taurus constellation and the fourteenth brightest star in the night sky. The name is Arabic and means 'the follower'.</p><table><tr><td>Apparent Magnitude</td><td>0.86</td></tr><tr><td>Spectral type</td><td>K5III</td></tr><tr><td>Mass</td><td>1.5 Solar Masses</td></tr><tr><td>Luminosity</td><td>518 Solar Luminosity</td></tr><tr><td>Distance from Earth</td><td>65.3ly / 20pc</td></tr><tr><td>Temperature</td><td>3910</td></tr></table>")
+      .fadeIn(3000);
+})
+$(".epsilon-tauri").click(function(){
+   $("#starinfo").html("<h4>Epsilon Tauri</h4><p>Epsilon Tauri is an orange giant star in the Taurus constellation, and it is also a member of the Hyades open cluster.</p><table><tr><td>Apparent Magnitude</td><td>3.53</td></tr><tr><td>Spectral type</td><td>K0III</td></tr><tr><td>Mass</td><td>2.7 Solar Masses</td></tr><tr><td>Luminosity</td><td>97 Solar Luminosity</td></tr><tr><td>Distance from Earth</td><td>147ly / 45pc</td></tr><tr><td>Temperature</td><td>4901</td></tr></table>")
+      .fadeIn(3000);
+})
+$(".t-tauri").click(function(){
+   $("#starinfo").html("<h4>T Tauri</h4><p>T Tauri is a variable star, and the prototype of the T Tauri stars. Though it appears from earth to be amongst the Hyades cluster, it is actually 420 light years behind it and was not formed with the rest of them.</p><table><tr><td>Apparent Magnitude</td><td>10.27</td></tr><tr><td>Spectral type</td><td>G5V:e</td></tr><tr><td>Mass</td><td>2.1 Solar Masses</td></tr><tr><td>Luminosity</td><td>n/a</td></tr><tr><td>Distance from Earth</td><td>600ly / 180pc</td></tr><tr><td>Temperature</td><td>n/a</td></tr></table>")
+      .fadeIn(3000);
+})
+$(".zeta-tauri").click(function(){
+   $("#starinfo").html("<h4>Zeta Tauri</h4><p>Zeta Tauri is a binary star in the Taurus constellation. The two components that make up Zeta Tauri are orbiting so close to each other that they cannot be resolved with a telescope.</p><table><tr><td>Apparent Magnitude</td><td>3.01</td></tr><tr><td>Spectral type</td><td>B2 IIIpe</td></tr><tr><td>Mass</td><td>11.2 Solar Masses</td></tr><tr><td>Luminosity</td><td>4169 Solar Luminosity</td></tr><tr><td>Distance from Earth</td><td>440ly / 140pc</td></tr><tr><td>Temperature</td><td>15500 K</td></tr></table>")
+      .fadeIn(3000);
+})
+$(".elnath").click(function(){
+   $("#starinfo").html("<h4>Elnath</h4><p>Elnath is the second brightest star in the Taurus constellation. The name is a reference to 'the butting' of the bull's horns.</p><table><tr><td>Apparent Magnitude</td><td>1.65</td></tr><tr><td>Spectral type</td><td>B7III</td></tr><tr><td>Mass</td><td>5.0 Solar Masses</td></tr><tr><td>Luminosity</td><td>700 Solar Luminosity</td></tr><tr><td>Distance from Earth</td><td>131ly / 40pc</td></tr><tr><td>Temperature</td><td>13824 K</td></tr></table>")
+      .fadeIn(3000);
 })
 
 
-/* 
-$("#clickme").click(function () {
-    $("#extra").fadeIn();
-});
-$(document).click(function (event) {
-    if (event.target.id !== 'clickme') {
-        $("#extra").fadeOut();
-    }
+
+
+
+
+/*$("h5").click(function(){
+   $("#starinfo")
+      .css('display=none');
+})
+
+$('#container').click(function (event) {
+   if (event.target.id !== "#starinfo") {
+      $("#starinfo").fadeOut();
+   }
 })
 */
 
